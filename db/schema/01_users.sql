ea0f19cf4ea7ef1df98c12904b08f088a7796aeb
ozgeguner <ozgecaylioglu@gmail.com>
2020-10-17 18:51:29 +0000

    created tables

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,40 @@
 -- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE favorite_maps (
+  id SERIAL PRIMARY KEY NOT NULL,
+  map_id INTEGER REFERENCES maps(id) ON DELETE CASCADE,
+  viewer_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE pins (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(150),
+  description TEXT,
+  longitude FLOAT,
+  latitude FLOAT,
+  image_url TEXT
+);
+
+CREATE TABLE maps (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  longitude FLOAT,
+  latitude FLOAT,
+  map_location_id INTEGER REFERENCES map_locations(id) ON DELETE CASCADE,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+);
+
+CREATE TABLE map_locations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  location_id INTEGER REFERENCES pins(id) ON DELETE CASCADE,
+  map_id INTEGER REFERENCES maps(id) ON DELETE CASCADE,
 );
