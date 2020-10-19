6d640e3b5bc58a7322555c616dcd8fe836875681
Etienne LC <etlecoent@gmail.com>
2020-10-19 04:03:26 +0000

    Moved a portion of the script to mapViewer

---
+++ public/scripts/maps.js
@@ -0,0 +1,29 @@
+// $(() => {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/maps"
+//   }).done((users) => {
+//     for(user of users) {
+//       $("<div>").text(user.name).appendTo($("body"));
+//     }
+//   });;
+// });
+
+// SERVER SIDE
+// WHEN GET ON /users/maps -> res.render SEND THE HTML
+
+// WHEN GET ON /api/maps -> res.send the data from the SQL request SEND THE DATA
+
+// USER SIDE
+// GET /users/maps then ASYNCH GET call on /example/maps to retrieve the data
+
+
+$(document).ready(() => {
+
+  $.get('/api/maps', data => {
+  }).then(data => {
+    for (let o of data) {
+      mapDrawer(o.id, o.title, [o.latitude, o.longitude]);
+    }
+  });
+});
