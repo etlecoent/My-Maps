19e0255ca5158176d840d4612e5e6120092c54c0
Etienne LC <etlecoent@gmail.com>
2020-10-20 20:58:53 +0000

    Added the image_url to the pinsDrawer function

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -10,9 +10,8 @@ $(document).ready(() => {
       mapsObj[m.id] = mapDrawer(m.id, m.title, [m.latitude, m.longitude]);
 
       $.get(`/api/pins/maps/${m.id}`).then(pins => {
-
         for (let p of pins) {
-          pinsDrawer(m.id, p.title, p.description, p.latitude, p.longitude, p.img_url)
+          pinsDrawer(m.id, p.title, p.description, p.latitude, p.longitude, p.image_url);
         }
       });
       addEditButton(m.id);
