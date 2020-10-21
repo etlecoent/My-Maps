b327964f70404a83481068ecb9d8e154acd5d0b3
Etienne LC <etlecoent@gmail.com>
2020-10-21 02:22:40 +0000

    Created the view, css & script for editing a map

---
+++ public/scripts/mapEditor.js
@@ -0,0 +1,23 @@
+let currentURL = $(location).attr('href');
+let apiURL = currentURL.replace('http://localhost:8080/users', '/api');
+
+const mapsObj = {};
+
+$(document).ready(() => {
+
+  $.get(`${apiURL}`).then(({maps:mapData, user_id}) => {
+
+    for (let m of mapData) {
+      addMapContainer(m.id);
+      addMapDiv(m.id);
+      mapsObj[m.id] = mapDrawer(m.id, m.title, [m.latitude, m.longitude]);
+
+      $.get(`/api/pins/maps/${m.id}`).then(pins => {
+
+        for (let p of pins) {
+          pinsDrawer(m.id, p.title, p.description, p.latitude, p.longitude, p.image_url)
+        }
+      });
+    }
+  });
+});
