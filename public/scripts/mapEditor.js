0fe57c743964cbaaef9242cf69caf499e75ebc9a
Etienne LC <etlecoent@gmail.com>
2020-10-22 19:45:41 +0000

    Temp code before Ozge's push

--- public/scripts/mapEditor.js
+++ public/scripts/mapEditor.js
@@ -1,50 +1,74 @@
 let currentURL = $(location).attr('href');
 let apiURL = currentURL.replace('http://localhost:8080/users', '/api');
 
+const mapsObj = {};
 
 $(document).ready(() => {
-
-  let mapsObj;
   let i = 0;
-  const markers = [];
+  let markers = [];
+
   $.get(`${apiURL}`).then(({maps:mapData, user_id}) => {
-    console.log(mapData)
     const {id, title, latitude, longitude} = mapData[0]
-      addMapContainer(id);
-      addMapDiv(id);
-    mapsObj = mapDrawer(id, title, [latitude, longitude]);
+    addMapContainer(id);
+    addMapDiv(id);
+    mapsObj[id] = mapDrawer(id, title, [latitude, longitude]);
+    $.get(`/api/pins/maps/${id}`).then(pins => {
+      for (let p of pins) {
 
-    // for (let m of mapData) {
-    //   addMapContainer(m.id);
-    //   addMapDiv(m.id);
+        pinsDrawer(id, p.title, p.description, p.latitude, p.longitude, p.image_url);
 
-    //   $.get(`/api/pins/maps/${m.id}`).then(pins => {
+      }
+    });
 
-    //     for (let p of pins) {
-    //       pinsDrawer(m.id, p.title, p.description, p.latitude, p.longitude, p.image_url)
-    //     }
-    //   });
-    // }
-    mapsObj.on("click", function(evt) {
-      console.log(evt.latlng)
-      var marker = L.marker([evt.latlng.lat, evt.latlng.lng]).addTo(mapsObj);
+    mapsObj[id].on("click", function(evt) {
+      let marker = L.marker([evt.latlng.lat, evt.latlng.lng]).addTo(mapsObj[id]);
       marker.bindPopup(`<form id="${i}">
-      <p>Name: <input type='text' name='marker_name'/></p>
-       <p><button>Add</button></p>
-   </form>`).openPopup();
-   $(`#${i}`).on("submit", function(evt) {
-     evt.preventDefault()
-     marker.name = evt.target.marker_name.value;
-     markers.push(marker)
-    //  mapsObj.closePopup()
-     console.log(markers)
-   })
-   i++
-    } )
-  });
-});
+      <p>Title: <input type='text' name='marker_title'/></p>
+      <p>Description: <input type='text' name='marker_description'/></p>
+      <p>Image Url: <input type='text' name='marker_image'/></p>
+        <button id="buttonSave" type="submit">Add</button>
+      </form>`).openPopup();
 
-// grab markes and send to back end to save
-// find id of map
-// markers table add name of marker, lat, long
-// reset and save button
+      $(`#${i}`).on("submit", function(evt) {
+        evt.preventDefault()
+        let markerTemp = {};
+
+        markerTemp.title = evt.target.marker_title.value;
+        markerTemp.detail = evt.target.marker_description.value;
+        markerTemp.image_url = evt.target.marker_image.value;
+        markerTemp.lat = marker._latlng.lat
+        markerTemp.lng = marker._latlng.lng
+        markers.push(markerTemp);
+        // Add data to the pins array
+        marker.closePopup();
+        console.log(markers);
+      })
+    });
+
+
+    // Save button which is gonna send the array of pins to the api
+    // Create save button then reference it in the ajax request below
+
+    $('#buttonUpdate').submit(function(event) {
+      event.preventDefault();
+      console.log('eventTriggered');
+      console.log(markers);
+      $.ajax({
+        method: "POST",
+        url: `${window.location.origin}/api/maps/${id}/pins/`,
+        data: {markers}
+      });
+    })
+  });
+
+
+  // Event triggered on button delete click that deletes the marker
+  $('#buttonDelete').on("click", function(event) {
+  //  event.preventDefault();
+      $.ajax({
+      method: "DELETE",
+      url: `${window.location.origin}/api/maps/${id}/pins/`,
+      data: selectedPin
+    });
+  })
+})
