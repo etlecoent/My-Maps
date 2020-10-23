34aa369c5dc26aab9cc98b764901f8e2f8c59387
Etienne LC <etlecoent@gmail.com>
2020-10-23 14:27:05 +0000

    Edited the data to be more consistent

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -3,4 +3,4 @@
 INSERT INTO maps (title, latitude, longitude, owner_id) VALUES
   ('Pizza Map of Montreal', 45.50884, -73.58781, 1),
   ('Hockey Arenas in Montreal', 45.50884, -73.58781, 2),
-  ('Cinema Map of Montreal', 45.50884, -73.58781, 3);
+  ('Pub Map of Montreal', 45.50884, -73.58781, 3);
